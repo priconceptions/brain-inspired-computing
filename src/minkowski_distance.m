f9c0370353050deff548c9066967ce2cef4d2e39
priyankadhulkhed <priyanka.dhulkhed@gmail.com>
2019-03-12 12:41:42 -0400

    new posts

--- src/minkowski_distance.m
+++
@@ -1,6 +0,0 @@
-function [ dist ] = minkowski_distance(v1, v2, r)
-%MINKOWSKI_DISTANCE: computes minkowski distance of two vectors
-%   Detailed explanation goes here
-dist  = (sum(abs(v1 - v2) .^ r)) ^ (1/r);
-end
-
